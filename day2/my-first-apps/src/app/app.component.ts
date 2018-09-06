59eda9d2eb2c1d0bcacba6df306d3051944b433f
zunym <zunyinmon@zunyinmon.com>
2018-09-06 15:29:38 +0800

    Commit workshop 2

---
+++ day2/my-first-apps/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { ToPrint } from './components/hello.component';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  listOfNames = ['A','B','C'];
+  toDisplay: string = 'Fred'
+
+  nameSelected(name: string) {
+    console.log('clicked: ', name);
+    this.toDisplay = name;
+  }
+
+  //name is $event - from toPrint.next(this.displayName)
+  pleasePrintThis(eventObject: ToPrint) {
+    console.log('>>> from app-hello: ', eventObject);
+
+  }
+}
