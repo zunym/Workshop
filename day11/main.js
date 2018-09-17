9812bd41e23aee5b91f25c9fd3b1b6fe81af17c8
zunym <zunyinmon@zunyinmon.com>
2018-09-17 13:27:45 +0800

    workshop day11

---
+++ day11/main.js
@@ -0,0 +1,36 @@
+//Load some libraries
+const path = require('path');
+const express = require('express');
+
+//Create an instance of express
+const app = express();
+
+//Run app / Open terminalS
+//nodemon will look for 
+//main attribute starting file in package.json [eg:main.js]
+
+
+app.use(
+    express.static(//middleware to serve static files
+        path.join(__dirname + '/public')
+    )
+); //any req come in use this rule to handle it
+
+app.use(
+    express.static(//middleware to serve static files
+        path.join(__dirname + '/media')
+    )
+);
+
+//Error Message Route to 404
+app.use((req, resp) => {//middleware
+    resp.status(404);
+    resp.sendfile(path.join(__dirname, 'media', '404.png'));
+
+});
+
+app.listen(3000, () => {
+    console.info("App started on port 3000");
+    console.info('running dir is', __dirname);
+    console.info('public dir is', path.join(__dirname + '/public'));
+})
